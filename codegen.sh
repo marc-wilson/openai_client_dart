6092426927310ebe23e661ba36e59331dddaf04f
Marc Wilson <marc@headset.io>
2024-01-31 19:44:50 -0800

    a

--- codegen.sh
+++ codegen.sh
@@ -1,5 +1,5 @@
 docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate \
     -i https://raw.githubusercontent.com/openai/openai-openapi/master/openapi.yaml \
     -g dart \
-    -o local/openai \
+    -o local \
     --skip-validate-spec
